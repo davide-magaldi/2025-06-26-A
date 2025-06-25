49f4cb8c0aa1f3b07e1de568f30f3011381a9b5a
Giuseppe Averta <g.averta3@gmail.com>
2025-06-25 13:31:23 +0200

    add DAO method

--- database/DAO.py
+++ database/DAO.py
@@ -2,5 +2,19 @@ from database.DB_connect import DBConnect
 
 
 class DAO():
-    def __init__(self):
-        pass
+    @staticmethod
+    def getAllCircuits():
+        cnx = DBConnect.get_connection()
+        cursor = cnx.cursor(dictionary=True)
+        query = """SELECT * 
+                    from circuits"""
+        cursor.execute(query)
+
+        res = []
+        for row in cursor:
+            res.append(row)
+
+        cursor.close()
+        cnx.close()
+        return res
+
